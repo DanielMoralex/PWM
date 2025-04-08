7fb2353fad773bb5ae08f797d412f8071fd605c4
Daniel <dmoralessosa@gmail.com>
2025-04-08 18:30:39 +0100

    Cambios para ver componentes.

--- sprint3/src/app/app.component.ts
+++ sprint3/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ColegioComponent } from './components/colegio/colegio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [ColegioComponent],
+  template: '<app-colegio></app-colegio>',
+  standalone: true,
 })
 export class AppComponent {
   title = 'sprint3';
--- sprint3/src/app/components/colegio/colegio.component.ts
+++ sprint3/src/app/components/colegio/colegio.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-colegio',
+  standalone: true,
   imports: [],
   templateUrl: './colegio.component.html',
   styleUrl: './colegio.component.css'
 })
-export class ColegioComponent {
+export class ColegioComponent implements OnInit {
+
+  MyVariable = 'myVariable';
+
+  constructor() {
+  }
+
+  ngOnInit(): void {}
 
 }
