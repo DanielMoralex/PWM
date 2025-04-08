d0a423933b7467591fc449898affb9c0fba02440
ivanp <ivanperezdiaz829@gmail.com>
2025-04-08 17:35:18 +0100

    Creación dentro del colegio

---
+++ sprint3/src/app/sprint3/src/app/components/colegio/colegio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colegio',
+  imports: [],
+  templateUrl: './colegio.component.html',
+  styleUrl: './colegio.component.css'
+})
+export class ColegioComponent {
+
+}
