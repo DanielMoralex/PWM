56869b1b2841c5db8be73b26c3fd0dc0caf97d37
Daniel <dmoralessosa@gmail.com>
2025-04-21 19:39:02 +0100

    Arreglo de versiones

--- sprint3/src/app/app.component.ts
+++ sprint3/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ColegioComponent } from './components/colegio/colegio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ColegioComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
---
+++ sprint3/src/app/components/colegio/colegio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colegio',
+  imports: [],
+  templateUrl: './colegio.component.html',
+  standalone: true,
+  styleUrl: './colegio.component.css'
+})
+export class ColegioComponent {}
