eae529fc6814b021b1cf23fc49632041ff888e9d
ivanp <ivanperezdiaz829@gmail.com>
2025-04-21 10:12:53 +0100

    Modificación del componente registro para adaptarse a los Mockups

--- sprint3/src/app/app.config.ts
+++ sprint3/src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { Routes, provideRouter } from '@angular/router';
 import { ColegioComponent } from './components/colegio/colegio.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ColegioComponent
+  },
+  {
+    path: 'registro',
+    component: RegistroComponent
   }
 ];
 
