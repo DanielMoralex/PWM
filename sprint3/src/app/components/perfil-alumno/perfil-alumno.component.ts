8a9c9948975842aca1ce25403a047d90f9a04304
ivanp <ivanperezdiaz829@gmail.com>
2025-05-04 18:36:43 +0100

    Ya funcionan los perfiles de usuario y mejora las noticias de la tienda

--- sprint3/src/app/components/perfil-alumno/perfil-alumno.component.ts
+++ sprint3/src/app/components/perfil-alumno/perfil-alumno.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-perfil-alumno',
   imports: [],
+  standalone: true,
   templateUrl: './perfil-alumno.component.html',
   styleUrl: './perfil-alumno.component.css'
 })
--- sprint3/src/app/components/perfil-profesor/perfil-profesor.component.ts
+++ sprint3/src/app/components/perfil-profesor/perfil-profesor.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-perfil-profesor',
   imports: [],
+  standalone: true,
   templateUrl: './perfil-profesor.component.html',
   styleUrl: './perfil-profesor.component.css'
 })
